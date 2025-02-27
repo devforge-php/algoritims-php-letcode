fa4720513d20554bf2b675bf74f0c31f94def13c
Azizbekweb098 <webcoderazizbek@gmail.com>
2025-02-27 23:11:38 +0500

    laravel

---
+++ task.php
@@ -0,0 +1,47 @@
+<?php 
+
+
+
+
+  
+
+// function random($massiv)
+// {
+// shuffle($massiv);
+// return $massiv;
+// }
+// $massiv = ['Azizbek', 'Jahon', 'Soliha', 'Kamron', 'bolta', 'Mubina', 'Abdulaziz', 'SaidAhmad'];
+
+// $randomMassiv = random($massiv);
+// $ch = implode(",", $randomMassiv);
+// echo $ch;
+
+// $names = [];
+
+// function nameFunction()
+// {
+//     global $names;
+//     for($i = 1; $i <= 5; $i++)
+// {
+//   $name = readline("5-ta isim kiriting ($i/5)");
+//   $names[] = $name;
+// }
+
+// }
+
+// nameFunction();
+
+// echo "Kiritilgan isimlar: ";
+// print_r($names);
+
+
+$array = [1,2,4,5];
+
+
+$srrt = array_filter($array, function ($arr){
+   
+});
+
+
+
+?>
