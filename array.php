fd557a68ed68c7249a05680f4697358a49a8b866
Azizbekweb098 <webcoderazizbek@gmail.com>
2025-02-27 21:14:10 +0500

    laravel

---
+++ array.php
@@ -0,0 +1,71 @@
+<?php
+
+
+// $furuits =  ['apple', 'banana',];
+
+// $str = implode(",", $furuits);
+
+
+
+// $yangiMassiv = explode(",", $str);
+
+// echo $yangiMassiv;
+
+////////// massiveni text aylantirish yoki string ga,
+
+
+// $sonlar = [1,2,3,4,5,6,7,8,9];
+
+// $str =  array_filter($numbers, function ($son) {
+//     return $son % 2 == 0;
+// });
+
+// $yangiMassiv = implode(",", $str);
+
+// echo $yangiMassiv;
+
+//////////  array filter massivlarni fillterlaydi keraklisini olbi qolsih uchun;
+
+
+// $sonlar = [1,2,3,4,5,6,7,8,9];
+// $kv = array_map(function($son){
+//    return $son * $son;
+// }, $sonlar);
+
+// print_r($kv);
+
+// ///////// array yana bir marta array yaratib ularni kopaytib chiqadi,
+
+
+
+// $sonlar = [1,2,3,4,5,6,7,8,9];
+// $yigindi = array_reduce($sonlar, function($carry, $item) {
+//     return $carry + $item;
+// });
+////// array_reduce masvie sonlar hammasini yigindinisi chiqaradi
+
+
+
+// function salom()
+// {
+//     echo "Salom php";
+// }
+// salom();
+
+
+// function counter()
+// {
+//     static $count = 0;
+//     $count++;
+//     echo $count;
+
+//     for($let = 0; $let < 120; $let++)
+//     {
+//   echo $count . "<br/>" . $let;
+//     }
+// }
+
+// counter();
+
+?>
+
